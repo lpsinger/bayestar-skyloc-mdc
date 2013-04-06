32569ce62811aa0480aad253482f9f7a496d5572
Leo Singer <leo.singer@ligo.org>
2013-04-05 22:41:52 -0700

    Paginate!

--- paginate_coincs.sql
+++ paginate_coincs.sql
@@ -5,15 +5,14 @@ CREATE TEMPORARY TABLE to_keep_coincs AS SELECT
     ON (cd.coinc_def_id = ce.coinc_def_id)
     WHERE cd.description =
     'sngl_inspiral<-->sngl_inspiral coincidences'
-    ORDER BY ce.event_id
+    ORDER BY ce.coinc_event_id
     LIMIT %limit%
     OFFSET %offset%;
 
 CREATE TEMPORARY TABLE to_delete_coincs AS SELECT
     coinc_event_id FROM coinc_event
     WHERE coinc_event_id NOT IN (SELECT coinc_event_id FROM to_keep_coincs)
-    AND coinc_def_id IN (SELECT coinc_def_id FROM coinc_definer
-    WHERE description = 'sngl_inspiral<-->sngl_inspiral coincidences');
+    AND coinc_def_id IN (SELECT coinc_def_id FROM coinc_definer);
 
 DELETE FROM coinc_inspiral
     WHERE coinc_inspiral.coinc_event_id IN (SELECT * FROM to_delete_coincs);
@@ -24,3 +23,5 @@ DELETE FROM coinc_event
 DELETE FROM coinc_event_map
     WHERE coinc_event_map.coinc_event_id IN (SELECT * FROM to_delete_coincs)
     OR coinc_event_map.event_id IN (SELECT * FROM to_delete_coincs);
+
+VACUUM;
