669680480e236851bd8467eed333ce2b655e8897
Leo Singer <leo.singer@ligo.org>
2013-05-13 14:03:38 -0700

    Fix egregious bugs in prune_min_far.sql

--- prune_min_far.sql
+++ prune_min_far.sql
@@ -1,23 +1,20 @@
 -- For each sim_inspiral record, for each 'sim_inspiral<-->coinc_event'
 -- association, select the one with the lowest false alarm rate.
-CREATE TEMPORARY TABLE to_keep_coincs AS SELECT
-    MIN(ci.combined_far) AS min_combined_far,
-    cem1.event_id AS simulation_id,
-    cem2.event_id AS coinc_event_id
-    FROM coinc_definer AS cd
-    INNER JOIN coinc_event AS ce
-    ON (cd.coinc_def_id = ce.coinc_def_id)
-    INNER JOIN coinc_event_map AS cem1
-    ON (cem1.coinc_event_id = ce.coinc_event_id)
+CREATE TEMPORARY TABLE to_keep_coincs AS SELECT DISTINCT
+    (SELECT ci.coinc_event_id FROM coinc_inspiral AS ci
     INNER JOIN coinc_event_map AS cem2
-    ON (cem2.coinc_event_id = ce.coinc_event_id)
-    INNER JOIN coinc_inspiral AS ci
-    ON (ci.coinc_event_id = cem2.event_id)
-    WHERE cd.description =
-    'sim_inspiral<-->coinc_event coincidences (nearby)'
-    AND cem1.table_name = 'sim_inspiral'
-    AND cem2.table_name='coinc_event'
-    GROUP BY cem1.event_id;
+    ON (cem2.event_id = ci.coinc_event_id)
+    INNER JOIN coinc_event_map AS cem1
+    ON (cem1.coinc_event_id = cem2.coinc_event_id)
+    INNER JOIN coinc_event AS ce
+    ON (ce.coinc_event_id = cem2.coinc_event_id)
+    INNER JOIN coinc_definer AS cd ON (ce.coinc_def_id = cd.coinc_def_id)
+    WHERE cem1.table_name = 'sim_inspiral'
+    AND cem2.table_name = 'coinc_event'
+    AND cem1.event_id = simulation_id
+    AND cd.description = 'sim_inspiral<-->coinc_event coincidences (nearby)'
+    ORDER BY ci.combined_far LIMIT 1)
+    AS coinc_event_id FROM sim_inspiral WHERE coinc_event_id IS NOT NULL;
 
 -- Make a list of all 'sim_inspiral<-->coinc_event' associations but those.
 CREATE TEMPORARY TABLE to_delete_coincs AS SELECT
